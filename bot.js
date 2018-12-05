7c41198f0f0000cc52b73948db1213bc5c56eb25
SpammmerBots <44922126+SpammmerBots@users.noreply.github.com>
2018-12-05 15:04:19 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4788,7 +4788,7 @@ client.on('message', message => {
                                    message.member.addRole(message.guild.roles.find("name", "• Mal »Top"));
                                    message.member.removeRole(message.guild.roles.find("name", "0"));
                                    msg.delete();
-                                   message.channel.send(`**•Done.**`).then(d => d.delete(100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000));
+                                   message.channel.send(`**•Done.**`).then(d => d.send(1));
 
                                    })
                                    })
