5016e86d39fae89243c0af9d82160058d9dd9e4e
SpammmerBots <44922126+SpammmerBots@users.noreply.github.com>
2018-12-07 12:57:46 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1041,16 +1041,16 @@ client.on('message', message => {
 ////////////////////////////////////////-----------////////////////////////////////////Ticket
 client.on("message", (message) => {
     /// ALPHA CODES
-   if (message.content.startsWith("g!ticket")) {     /// ALPHA CODES
+   if (message.content.startsWith("g!testnew")) {     /// ALPHA CODES
         const reason = message.content.split(" ").slice(1).join(" ");     /// ALPHA CODES
         if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
+        if (message.guild.channels.exists("name", "ticket-${x}" + message.author)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
         message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
             let role = message.guild.roles.find("name", "Support Team");
             let role2 = message.guild.roles.find("name", "@everyone");
             c.overwritePermissions(role, {
                 SEND_MESSAGES: true,
-                READ_MESSAGES: true
+                READ_MESSAGES: true/////////////// ticket-{message.author.id} ticket-{message.author.id}
             });    /// ALPHA CODES
             c.overwritePermissions(role2, {
                 SEND_MESSAGES: false,
@@ -1117,7 +1117,7 @@ client.on("message", (message) => {
                 SEND_MESSAGES: true,
                 READ_MESSAGES: true
             });
-            message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
+            message.channel.send(`:white_check_mark: Your ticket has been created, #${x}`);
             const embed = new Discord.RichEmbed()
                 .setColor(0xCF40FA)
                 .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Staff** will be here soon to help.`)
@@ -4722,7 +4722,7 @@ function Days(date) {
 client.on('message', message => {//GAMING
     if(!message.channel.guild) return;//GAMING
 var prefix = "g!";//GAMING
-if(message.content.startsWith(prefix + 'channel')) {//Alpha Codes
+if(message.content.startsWith(prefix + 'channel')) {//
     let channel = message.channel//GAMING
     var embed = new Discord.RichEmbed()//GAMING
       .setTitle(":low_brightness:Channel Info:small_orange_diamond: ")//GAMING
