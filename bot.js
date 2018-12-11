0830863fc336377f75208ada1e3e8ecc1933528d
SpammmerBots <44922126+SpammmerBots@users.noreply.github.com>
2018-12-11 04:21:51 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1103,7 +1103,7 @@ client.on("message", (message) => {
    if (message.content.startsWith("g!new")) {     /// Mal 
         const reason = message.content.split(" ").slice(1).join(" ");     /// Mal
         if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-        if (message.guild.channels.exists("name", "<ticket-{message.author.id}>" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
+        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
         message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
             let role = message.guild.roles.find("name", "Support Team");
             let role2 = message.guild.roles.find("name", "@everyone");
@@ -1119,7 +1119,7 @@ client.on("message", (message) => {
                 SEND_MESSAGES: true,
                 READ_MESSAGES: true
             });
-            message.channel.send(`:white_check_mark: **Your Ticket Has Been Created**,<${args}>`);
+            message.channel.send(`:white_check_mark: **Your Ticket Has Been Created**,#{c.name}`);
             const embed = new Discord.RichEmbed()
                 .setColor(0xCF40FA)
                 .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Staff** will be here soon to help.`)
