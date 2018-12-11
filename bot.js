09830485e24c0bd55e6b3140c5248e09a666426b
SpammmerBots <44922126+SpammmerBots@users.noreply.github.com>
2018-12-11 05:12:32 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1062,7 +1062,7 @@ client.on("message", (message) => {
                 SEND_MESSAGES: true,
                 READ_MESSAGES: true
             });
-            message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
+            message.channel.send(`:white_check_mark: Your ticket has been created, <#${msg.author.id}>`);
             const embed = new Discord.RichEmbed()
                 .setColor(0xCF40FA)
                 .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Staff** will be here soon to help.`)
@@ -1119,7 +1119,7 @@ client.on("message", (message) => {
                 SEND_MESSAGES: true,
                 READ_MESSAGES: true
             });
-            message.channel.send(`:white_check_mark: **Your Ticket Has Been Created**,<#{message.author.id}>`);
+            message.channel.send(`:white_check_mark: **Your Ticket Has Been Created**,#{c.name}`);
             const embed = new Discord.RichEmbed()
                 .setColor(0xCF40FA)
                 .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Staff** will be here soon to help.`)
