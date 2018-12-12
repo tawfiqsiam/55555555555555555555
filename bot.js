b895d80aadbbe490cea936ccdc09e8a239d5d270
tawfiqsiam <toto.siam999@gmail.com>
2018-12-12 14:35:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const tpoints = {};
 const vpoints = {};
 const dateFormat = require('dateformat');
 const fs = require("fs"); 
-const prefix = "." // برفكس
 let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
 const Canvas = require("canvas"); //npm i canvas
 const jimp = require("jimp");
