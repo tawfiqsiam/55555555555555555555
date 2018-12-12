58cc91ff52ab6796b15a6405371541062fe29a6f
tawfiqsiam <toto.siam999@gmail.com>
2018-12-12 14:37:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const Discord = require('discord.js');
 const tpoints = {};
 const vpoints = {};
 const dateFormat = require('dateformat');
-const fs = require("fs"); 
 let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
 const Canvas = require("canvas"); //npm i canvas
 const jimp = require("jimp");
