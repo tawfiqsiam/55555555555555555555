df29a184d898ee707d3128500e32e11a7c1b5732
tawfiqsiam <toto.siam999@gmail.com>
2018-12-13 14:23:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8229,7 +8229,7 @@ client.on('guildMemberAdd', member => {
     const ei = invites[member.guild.id];
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("Welcome", "↗wèlcòmèヅ");
+    const stewart = member.guild.channels.find("Welcome", "welcome");
      stewart.send(`<@${member.user.id}> Invite By: <@${inviter.id}>`);
    // stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
   }); 
