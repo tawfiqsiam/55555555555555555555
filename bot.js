33b3da1e14664bef86fdfb414582db947ce3a4b0
tawfiqsiam <toto.siam999@gmail.com>
2018-12-17 10:05:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7972,63 +7972,6 @@ const Discord = require('discord.js');
 	
 	
 	
-	const w = ['./img/w1.png'];
-	
-	         let Image = Canvas.Image,
-	            canvas = new Canvas(400, 200),
-	            ctx = canvas.getContext('2d');
-	        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-	            if (err) return console.log(err);
-	            let BG = Canvas.Image;
-	            let ground = new Image;
-	            ground.src = Background;
-	            ctx.drawImage(ground, 0, 0, 400, 200);
-	             
-	          
-	
-	                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(100) + ".png" : member.user.displayAvatarURL;
-	                jimp.read(url, (err, ava) => {
-	                    if (err) return console.log(err);
-	                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-	                        if (err) return console.log(err);
-	                        
-	                        ctx.font = "bold 12px Arial";
-	                        ctx.fontSize = '20px';
-	                        ctx.fillStyle = "#f1f1f1";
-	                        ctx.textAlign = "center";
-	                        ctx.fillText(`welcome to ${member.guild.name}`, 300, 130);
-	                        
-	                        ctx.font = "bold 12px Arial";
-	                        ctx.fontSize = '20px';
-	                        ctx.fillStyle = "#f1f1f1";
-	                        ctx.textAlign = "center";
-	                        ctx.fillText(member.user.username, 200, 150);
-	 
-	                let Avatar = Canvas.Image;
-	                              let ava = new Avatar;
-	                              ava.src = buf;
-	                              ctx.beginPath();
-	                              ctx.arc(77, 101, 62, 0, Math.PI*2);
-	                              ctx.stroke();
-	                                 ctx.clip();
-	                                 ctx.drawImage(ava, 13, 38, 128, 126);  
-	                          
-	                
-	                             
-	welcomer.sendFile(canvas.toBuffer())
-	
-	
-	
-	      
-	      
-	                    }  )  
-	      
-	                    
-	
-	})
-	      });                    
-	 }
-	});
 	///////By:Mahmoud-QuaStyle
 	
 	client.on('message',async message => {
