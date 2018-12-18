89119566176c38e973b13fc1239fdbfc38397a79
tawfiqsiam <toto.siam999@gmail.com>
2018-12-18 09:08:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2135,30 +2135,33 @@ m.sendMessage(args)
 	
 	
 	client.on('message', msg => {
-		var prefix = "!";
-	  if (msg.author.bot) return;
-	  if (!msg.content.startsWith(prefix)) return;
-	  let command = msg.content.split(" ")[0];
-	  command = command.slice(prefix.length);
-	  let args = msg.content.split(" ").slice(1);
-	
-	    if(command === "clear") {
-	        const emoji = client.emojis.find("name", "wastebasket")
-	    let textxt = args.slice(0).join("");
-	    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
-	    if (textxt == "") {
-	        msg.delete().then
-	    msg.channel.send("***```how many MESSAGES do u want to clear 👌```***").then(m => m.delete(3000));
-	} else {
-	    msg.delete().then
-	    msg.delete().then
-	    msg.channel.bulkDelete(textxt);
-	        msg.channel.send("```php\n cleared MESSAGES: " + textxt + "\n```").then(m => m.delete(3000));
-	        }    
-	    }
-	}
-	});
-	
+	var prefix = "!";
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+
+    if(command === "clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***```how many do u want to clear 👌```***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```php\ncleard: " + textxt + "\n```").then(m => m.delete(3000));
+        }    
+    }
+}
+});
+
+
+/////////////////-----------//////////////////////////////// //////////////////////////////-----------/////////////////////emojilist
+
 	
 	/////////////////-----------//////////////////////////////// //////////////////////////////-----------/////////////////////emojilist
 	
