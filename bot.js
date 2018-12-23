7c604e36ff3f872eabedaf4cc068993c8086ed8c
tawfiqsiam <toto.siam999@gmail.com>
2018-12-23 14:34:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4585,19 +4585,7 @@ client.on('message', msg => {
 	
 	
 	///////////////////////////////////////////
-	client.on('message', function(message) {
-	    if (message.channel.type === "dm") {
-	        if (message.author.id === client.user.id) return;
-	        var iiMo = new Discord.RichEmbed()
-	        .setColor('RANDOM')
-	        .setTimestamp()
-	        .setTitle('``I have received a new DM !``')
-	        .setThumbnail(`${message.author.avatarURL}`)
-	        .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
-	        .setFooter(`From **${message.author.tag} (${message.author.id})**`)
-	    client.channels.get("513482762775429139").send({embed:iiMo});
-	    }
-	});
+	
 	
 	////////////////RsaLa most5dem el BOT
 	
