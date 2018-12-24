19407bb3cd3fc3d402ca2eb9b1c32f146a817da2
tawfiqsiam <toto.siam999@gmail.com>
2018-12-24 08:53:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5119,7 +5119,7 @@ client.on('message', msg => {
 	    if (message.author.bot) return;
 	    if (message.isMentioned(client.user))
 	    {
-	    message.reply("`!help !help-R`");
+	    message.reply("`!help `");
 	    }
 	});
 	
@@ -5671,42 +5671,28 @@ client.on('message', msg => {
 	
 	/////////////-------------------------------/////امثال
 	client.on('message', message => {
-	    if (message.content == "!امثال") {
-	        var x = ["أذا ذل رويال فهو ...",
-	"الإتحاد ...",
-	"الناس سواسية كأسنان ...",
-	"ما أشبه الليله",
-	"البعد ...",
-	"الماء أهون موجود وأعز ...",
-	"الهزيمة تحل ...",
-	"العقل ...",
-	"البطنة تزيل ...",
-	"اللبيب بالإشارة ...",
-	"اخطب لابنتك ولا تخطب ...",
-	"أعز من الولد ولد ...",
-	"القرد في عين أمه ...",
-	"الكتاب يقرأ من ...",
-	"آخر الحياة ...",
-	"أكرم نفسك عن كل",
-	"العز في نواصي",
+	    if (message.content == "#QH") {
+	        var x = ["What is the capital city of Spain? ...",
+	"Which is the only American state to begin with the letter [p] ...",
+	"Name the world's biggest island.  ...",
+	"What is the world's longest river?",
+	"Name the world's largest ocean. ...",
+	"What is the diameter of Earth?  ...",
+	"Which country is Prague in ...",
+	"Who played Neo in The Matrix?  ...",
+	"In which film did Humphrey Bogart say, {We'll always have Paris?} ...",
+	
 	];
-	        var x2 = ['ذليل',
-	        "قوة",
-	        "المشط",
-	        "بالبارحة",
-	        "جفاء",
-	        "مفقود",
-	        "العزيمة",
-	        "زينة",
-	        "الفطنة",
-	        "يفهم",
-	        "لابنك",
-	        "الولد",
-	        "غزال",
-	        "عنوانه",
-	        "الموت",
-	        "دنيء",
-	        "الخيل",
+	        var x2 = ['Madrid',
+	        "Pennsylvania",
+	        "Greenland",
+	        "Amazon",
+	        "Pacific",
+	        "8,000 miles",
+	        "Czech Republic",
+	        "Keanu Reeves",
+	        "Casablanca",
+	     
 	        
 	        
 	        
@@ -5714,20 +5700,20 @@ client.on('message', msg => {
 	        ];
 	        
 	        var x3 = Math.floor(Math.random()*x.length)
-	        message.channel.send(` اكمل المثل التآلي :  __**${x[x3]}**__ ؟
-	    لديك 30 ثآنية للإجآبة `).then(msg1=> {
+	        message.channel.send(` answer the Qestuoin :  __**${x[x3]}**__ ؟
+	    you have 10 sec to answer`).then(msg1=> {
 	            var r = message.channel.awaitMessages(msg => msg.content == x2[x3], {
 	                maxMatches : 1,
-	                time : 30000,
+	                time : 10000,
 	                errors : ['time']
 	            })
 	        r.catch(() => {
-	            return message.channel.send(`:negative_squared_cross_mark: لقد انتهى الوقت ولم يقم أحد بالأجابة بشكل صحيح 
-	            الإجآبة الصحيحةة هي __**${x2[x3]}**__`)
+	            return message.channel.send(`:negative_squared_cross_mark: the time is up
+	            the right answer is  __**${x2[x3]}**__`)
 	        })
 	        
 	        r.then((collected)=> {
-	            message.channel.send(`${collected.first().author} لقد قمت بكتابة الجواب الصحيح بالوقت المناسب  `);
+	            message.channel.send(`${collected.first().author} well done!  `);
 	        })
 	        })
 	    }
