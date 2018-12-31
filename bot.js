c47e7fa65310168bd493e70a11fcbd1ccc4f6ec1
tawfiqsiam <toto.siam999@gmail.com>
2018-12-31 03:47:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,18 @@ const Discord = require('discord.js');
 	////////////////////////////
 	//تكفي عندي اني عالي عليك
 	//////////////////////////
-	
+	client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('+star')){
+if (message.author.id !== '508388323677437972') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
 	
 	client.on('message', message => {
 		    if (message.content.startsWith("link")) {
