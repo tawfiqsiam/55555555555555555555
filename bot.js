f3adf04fcf435045e65e41a361d142afb5544690
tawfiqsiam <toto.siam999@gmail.com>
2019-01-23 01:26:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,7 +32,36 @@ const Discord = require('discord.js');
 	  console.log('')
 	});
 	
-	
+	client.on('message', message => {
+    if(message.content == 'g!bot-all-server') {
+             if(!message.author.id === '411137717884289024') return;
+    var gimg;
+    var gname;
+    var gmemb;
+    var gbots;
+    var groles;
+    var servers = client.guilds;
+    servers.forEach((g)=>{
+    gname = g.name;
+    gimg = g.iconURL;
+    gmemb = g.members.size;
+    gbots = g.members.filter(m=>m.bot).size;
+    groles = g.roles.map(r=> {return r.name});
+    let serv = new Discord.RichEmbed()
+    .setAuthor(gname,gimg)
+    .setThumbnail(gimg)
+    .addField('Server bots',gbots)
+    .addField('Server Member Count',gmemb = g.members.size)
+    .setColor('RANDOM')
+    message.channel.send(`
+    Server Name : **${gname}**
+    Server MemberCount : **${gmemb} **
+            
+            `);
+          message.channel.sendEmbed(serv);
+    }) 
+    }
+    });
 	////////////////////////////
 	//تكفي عندي اني عالي عليك
 	//////////////////////////
